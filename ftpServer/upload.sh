1ec9de97f6d9dbb3353755ff77414a6bcaa3365e
RetepRelleum <retep.relleum@bluewin.ch>
2020-03-07 07:30:15 +0100

    s

--- ftpServer/upload.sh
+++ ftpServer/upload.sh
@@ -1,10 +1,23 @@
 HOST=esp32.home
 USER=user
 PASSWORD=pw
- 
+DIRECTORY="/lib"
+
+echo "mkdir"
+echo "***************"
+
 ftp -inv $HOST <<EOF
 user $USER $PASSWORD
-cd /lib
+mkdir $DIRECTORY
+bye
+EOF
+
+echo "Copy file"
+echo "***************"
+
+ftp -inv $HOST <<EOF
+user $USER $PASSWORD
+cd $DIRECTORY
 mput ftpServer.py
 bye
 EOF
